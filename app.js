5b588d2f559642e24e8c1da235cea3abf8ea986e
unknown <merykozan@gmail.com>
2025-01-05 16:42:35 +0100

    first file in the app

---
+++ app.js
@@ -0,0 +1,25 @@
+import express, { urlencoded, static } from 'express';
+import { connect } from 'mongoose';
+import { config } from 'dotenv';
+import { join } from 'path';
+
+config();
+const app = express();
+
+// Configuration de MongoDB
+connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+// Middlewares
+app.use(urlencoded({ extended: true }));
+app.use(static(join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+// Routes
+app.use('/', require('./routes/index'));
+app.use('/booking', require('./routes/booking'));
+
+// Lancement du serveur
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
